abc819cb09410a66ff0cb55a5550d675b6c03676
abhijeetkumar193 <52517930+abhijeetkumar193@users.noreply.github.com>
2021-04-18 19:55:48 +0530

    updated in master
    updated in master

--- src/FirstProgram.java
+++ src/FirstProgram.java
@@ -3,5 +3,6 @@ public class FirstProgram {
 public static void main(String[] args) {
 	System.out.println("first line in the java program");
 	System.out.println("second line in the java program");
+	System.out.println("Third line in the java program");
 }
 }
